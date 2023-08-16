3dc99bf817a6d71d8aec74582cb7b821ff3fa951
Rebecca Hass <rebecca.hass91@googlemail.com>
2023-08-16 17:33:37 +0200

    test:task 1

---
+++ operators-and-data-types/script.js
@@ -0,0 +1,19 @@
+console.log("R" + "e" + "b" + "e" + "c" + "c" + "a");
+
+// Error! Make it work
+console.log("Hallo");
+
+// Error! Make it work
+console.log("That doesn`t work");
+
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 === 52);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+
+//  Create a console.log that returns true when both division results are equal to 5
+console.log(25 / 5 === 50 / 10); // Result: 5
