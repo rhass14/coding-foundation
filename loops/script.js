e11fa8922bd9cd18adfb23d49ae58af21bbce1f3
Rebecca Hass <rebecca.hass91@googlemail.com>
2023-08-24 17:53:34 +0200

    init commit

---
+++ loops/script.js
@@ -0,0 +1,15 @@
+// oddNumbers
+function oddNumbers(a, b) {
+  const stringOddNumbers
+
+  for (let i = 0; i < b; i++) {
+    if (i < 2) console.log(oddNumbers(0, 4));
+  }
+}
+
+// The function has two parameters, both are numbers.
+//  Iterate over all numbers between first number and the second number.
+// Store all oddNumbers in string, seperated by a comma and return the string.
+// Allowed are only positive numbers, add a check.
+// result should be: 1,3
+
